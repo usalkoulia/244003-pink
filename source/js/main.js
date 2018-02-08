d81e597effcd772c9ed2cd53b51f31a8f13842b3
Ulia Usalko <usalko.ulia@gmail.com>
2018-02-09 01:36:24 +0200

    Работа с пп

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 var header_toggle = document.querySelector(".header__menu-toggle");
 var header = document.querySelector(".header");
 
+var slider_button = document.querySelector(".slider__button");
+var slider_item = document.querySelector(".slider__item");
+
 header_toggle.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (header.classList.contains("header--active")) {
